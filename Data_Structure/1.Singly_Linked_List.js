d428c19da09c1fd3592b10a87e25b3095c05c1cb
tesla <akilahmed096@gmail.com>
2020-12-17 02:23:55 +0530

    singly linked list completed

--- Data_Structure/1.Singly_Linked_List.js
+++ Data_Structure/1.Singly_Linked_List.js
@@ -70,6 +70,84 @@ class SinglyLinkedList {
     this.length++
     return this
   }
+
+  get(ind) {
+    if (ind < 0 || ind > this.length) {
+      return null
+    } else {
+      let count = 0
+      let val = this.head
+      while (count !== ind) {
+        val = val.next
+        count++
+      }
+
+      return val
+    }
+  }
+
+  set(ind, val) {
+    let currentVal = this.get(ind)
+
+    if (!currentVal) {
+      return false
+    } else {
+      currentVal.val = val
+      return true
+    }
+  }
+
+  insert(ind, val) {
+    if (ind < 0 || ind > this.length) return false
+    if (ind === this.length) return this.push(val)
+    if (ind === 0) return this.unshift(val)
+
+    let prev = this.get(ind - 1)
+    let newNode = new Node(val)
+
+    let temp = prev.next
+    prev.next = newNode
+    newNode.next = temp
+
+    this.length++
+
+    return this
+  }
+
+  remove(ind) {
+    if (ind < 0 || ind >= this.length) return false
+    if (ind === this.length - 1) return this.pop()
+    if (ind === 0) return this.shift()
+
+    let current = this.get(ind)
+    this.get(ind - 1).next = this.get(ind + 1)
+    this.length--
+
+    return current
+  }
+
+  reverse() {
+    let node = this.head
+    this.head = this.tail
+    this.tail = node
+
+    let next
+    let prev = null
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+
+    return this
+  }
 }
 
 let list = new SinglyLinkedList()
+
+list.push('world')
+list.push('worl')
+list.push('wor')
+list.push('wo')
